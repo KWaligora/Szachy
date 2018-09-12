c35c3049d94c50327e0d6ac8b218cd423a2449b9
Mromru <t.piekarczyk97@gmail.com>
2018-09-12 21:00:05 +0200

    Initial Commit
    Added project

---
+++ Szachy/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szachy
+{
+    class Entry
+    {
+        static int Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
