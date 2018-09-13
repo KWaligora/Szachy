396edb00e48e6b0955b4273f312bec8870477f8b
Mromru <t.piekarczyk97@gmail.com>
2018-09-13 20:42:58 +0200

    added chatLog textbox

--- Szachy/Window.cs
+++ Szachy/Window.cs
@@ -14,7 +14,7 @@ namespace Szachy
         private Panel board;
         Bitmap selectBitmap;
         int selectX, selectY;
-
+        private TextBox chatLog;
         Board boardModel;
 
         public Window(Board boardModel)
@@ -26,6 +26,7 @@ namespace Szachy
         private void InitializeComponent()
         {
             this.board = new System.Windows.Forms.Panel();
+            this.chatLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // board
@@ -37,15 +38,26 @@ namespace Szachy
             this.board.TabIndex = 0;
             this.board.Paint += new System.Windows.Forms.PaintEventHandler(this.board_Paint);
             this.board.MouseClick += new System.Windows.Forms.MouseEventHandler(this.board_MouseClick);
-
-            this.selectBitmap = global::Szachy.Properties.Resources.select;
+            // 
+            // chatLog
+            // 
+            this.chatLog.Location = new System.Drawing.Point(600, 300);
+            this.chatLog.Multiline = true;
+            this.chatLog.Name = "chatLog";
+            this.chatLog.Size = new System.Drawing.Size(200, 280);
+            this.chatLog.TabIndex = 1;
             // 
             // Window
             // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.chatLog);
             this.Controls.Add(this.board);
             this.Name = "Window";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
+            selectBitmap = global::Szachy.Properties.Resources.select;
             selectX = 0;
             selectY = 0;
         }
