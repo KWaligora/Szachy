aa74cd16c6b7ec854a2eaed557b26c083e8c6909
Mromru <t.piekarczyk97@gmail.com>
2018-09-13 16:07:29 +0200

    Revert "classes"
    This reverts commit fc527f191fa4b4e285cccc1e728c3b84ca3a1dea.

--- Szachy/Entry.cs
+++ Szachy/Entry.cs
@@ -12,36 +12,36 @@ namespace Szachy
        
         public static int Main(String[] args)
         {
-            /* Connection connection;
+            Connection connection;
+            
+            switch (args[0].ToUpper()) {
+                case "SERVER":
+                connection = new Connection(true, args[1], short.Parse(args[2]));
+                    break;
+                case "CLIENT":
+                connection = new Connection(false, args[1], short.Parse(args[2]));
+                    break;
+                default:
+                    Console.WriteLine("CRITICAL ERROR: args[0] is invalid; press any key to exit...");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                    connection = null; //otherwise you get using uninitialized variable error lol
+                    break;
+            }
 
-             switch (args[0].ToUpper()) {
-                 case "SERVER":
-                 connection = new Connection(true, args[1], short.Parse(args[2]));
-                     break;
-                 case "CLIENT":
-                 connection = new Connection(false, args[1], short.Parse(args[2]));
-                     break;
-                 default:
-                     Console.WriteLine("CRITICAL ERROR: args[0] is invalid; press any key to exit...");
-                     Console.ReadKey();
-                     Environment.Exit(1);
-                     connection = null; //otherwise you get using uninitialized variable error lol
-                     break;
-             }
+            if (connection.isHost)
+            {
+                connection.send("Hello there!");
+                connection.receive();
+            }
+            else
+            {
+                connection.receive();
+                connection.send("Hello too!");
+            }
 
-             if (connection.isHost)
-             {
-                 connection.send("Hello there!");
-                 connection.receive();
-             }
-             else
-             {
-                 connection.receive();
-                 connection.send("Hello too!");
-             }
-             */
-            Board b;
             Application.Run(new Window());
+
             Console.ReadKey();
             return 0;
         }
