eb108b408d10ba24ba65dab7bca328ed4e7446ec
Mromru <t.piekarczyk97@gmail.com>
2018-09-13 16:10:12 +0200

    Revert "Revert "classes""
    This reverts commit aa74cd16c6b7ec854a2eaed557b26c083e8c6909.

---
+++ Szachy/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szachy
+{
+    class Board
+    {
+        byte[] board = new byte[64];
+        public Board()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                board[i] = Convert.ToByte(i + 1);
+            }
+
+            for (int i = 48; i < 64; i++)
+            {
+                board[i] = Convert.ToByte(i - 31);
+            }
+
+            string test = "";
+            for (int i = 0; i<64; i++)
+            {
+                test += board[i];
+            }
+            MessageBox.Show(test);
+        }
+    }
+}
