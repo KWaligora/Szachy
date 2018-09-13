f4080478b22ddfac795efde8342be79754c06a9f
KWaligora <35140481+KWaligora@users.noreply.github.com>
2018-09-14 00:46:16 +0200

    warunki poruszania 4

--- Szachy/Pieces/Tower.cs
+++ Szachy/Pieces/Tower.cs
@@ -18,9 +18,9 @@ namespace Szachy.Pieces
             int xq = x % 8;
             int yp = y / 8;
             int yq = y % 8;
-            if (Math.Abs(xp - yp) == 0 || Math.Abs(xq - yq) == 0) return true;
+            if (board[x] < 17 && board[y] < 17) return false;
             if (board[x] > 16 && board[x] < 32 && board[y] > 16 && board[y] < 32) return false;
-            if (Math.Abs(xp - yp) == Math.Abs(xq - yq)) return true;
+            if (Math.Abs(xp - yp) == 0 || Math.Abs(xq - yq) == 0) return true;
             return false;
         }
 
