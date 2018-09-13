ce4e4eb81ebf957e8903fe36ec86ded81fbd5e63
Mromru <t.piekarczyk97@gmail.com>
2018-09-13 13:12:22 +0200

    Added panel for board

--- Szachy/Window.cs
+++ Szachy/Window.cs
@@ -5,10 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Szachy
 {
     class Window : Form
     {
+        private Panel board;
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.board = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.Location = new System.Drawing.Point(0, 0);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(600, 600);
+            this.board.TabIndex = 0;
+            // 
+            // Window
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.board);
+            this.Name = "Window";
+            this.ResumeLayout(false);
+
+        }
     }
 }
