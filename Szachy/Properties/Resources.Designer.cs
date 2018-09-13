ea7c5c95d529f6c2793227e5c3c20d256aebaf00
Mromru <t.piekarczyk97@gmail.com>
2018-09-13 16:07:26 +0200

    Revert "Merge branch 'master' of https://github.com/KWaligora/Szachy"
    This reverts commit 5c3a6db85a47c0c637205d6b473817c3d35a4a5b, reversing
    changes made to fc527f191fa4b4e285cccc1e728c3b84ca3a1dea.

--- Szachy/Properties/Resources.Designer.cs
+++ Szachy/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace Szachy.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        public static System.Drawing.Bitmap select {
-            get {
-                object obj = ResourceManager.GetObject("select", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
