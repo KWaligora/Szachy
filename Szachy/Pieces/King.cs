7d8623c10fd8ab7b057abc75e5fd2317ae7bf9f0
KWaligora <35140481+KWaligora@users.noreply.github.com>
2018-09-13 18:26:07 +0200

    Pieces classes

---
+++ Szachy/Pieces/King.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Szachy.Pieces
+{
+    class King: Piece
+    {
+        public King(byte id, Bitmap image) : base(id, image) { } //moze pozostac pusty
+
+        public override bool move()
+        {
+            return true;
+        }
+
+        public override bool special()
+        {
+            return true;
+        }
+
+        public override byte getPieceID()
+        {
+            return id;
+        }
+    }
+}
+
---
+++ Szachy/Pieces/Runner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Szachy.Pieces
+{
+    class Runner: Piece
+    {
+        public Runner(byte id, Bitmap image) : base(id, image) { } //moze pozostac pusty
+
+        public override bool move()
+        {
+            return true;
+        }
+
+        public override bool special()
+        {
+            return true;
+        }
+
+        public override byte getPieceID()
+        {
+            return id;
+        }
+    }
+}
