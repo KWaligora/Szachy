80df8e179320c56626474ebd583a8b9ebc2eb759
rox02 <szymonsol@op.pl>
2018-09-14 05:54:59 +0200

    1. Zrobilem ruchy dla wszystkich figur(bez kolizji po drodze).
    2. TABLICE NUMERUJA SIE OD ZERA!!!

--- Szachy/Pieces/King.cs
+++ Szachy/Pieces/King.cs
@@ -14,7 +14,16 @@ namespace Szachy.Pieces
 
         public override bool move(byte x, byte y)
         {
-            return true;
+            int xp = x / 8;
+            int xq = x % 8;
+            int yp = y / 8;
+            int yq = y % 8;
+
+            if (board[x] < 16 && board[y] < 16) return false;
+            if (board[x] > 16 && board[x] < 32 && board[y] > 16 && board[y] < 32) return false;
+            if (Math.Abs(xp - yp) == 1 || Math.Abs(xq - yq) == 1) return true;
+            
+            return false;
         }
 
         public override bool special()
--- Szachy/Pieces/Queen.cs
+++ Szachy/Pieces/Queen.cs
@@ -18,7 +18,7 @@ namespace Szachy.Pieces
             int xq = x % 8;
             int yp = y / 8;
             int yq = y % 8;
-            if (board[x] < 17 && board[y] < 17) return false;
+            if (board[x] < 16 && board[y] < 16) return false;
             if (board[x] > 16 && board[x] < 32 && board[y] > 16 && board[y] < 32) return false;
             if ((Math.Abs(xp - yp) == 0 || Math.Abs(xq - yq) == 0) || (Math.Abs(xp - yp) == Math.Abs(xq - yq))) return true;
 
